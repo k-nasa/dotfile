2dce9d70e32b285a1f600d2dd6d492a799aa0c12
asan kondo <htilcs1115@gmail.com>
2018-01-30 11:10:30 +0900

    インストールコマンド編集

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,3 +1,4 @@
+cd ~
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
