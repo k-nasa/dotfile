60bd1e59f1dee79716947db5ca3ed00bff9ac5d8
k-nasa <htilcs1115@gmail.com>
2019-09-16 18:07:36 +0900

    add git commit template

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -5,3 +5,5 @@ ln -sf ~/dotfile/.tmux.conf ~/
 ln -sf ~/dotfile/.zprofile ~/
 ln -sf ~/dotfile/.aliases ~/
 ln -sf ~/dotfile/.gitignore_global ~/
+ln -sf ~/dotfile/.gitconfig ~/
+ln -sf ~/dotfile/.commit_template ~/
