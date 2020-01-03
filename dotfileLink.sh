1966f6a4dca120e425c9bd0f2f728162f246533b
k-nasa <htilcs1115@gmail.com>
2020-01-03 11:15:10 +0900

    hoge

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,7 +1,6 @@
 ln -sf ~/dotfile/nvim ~/.config/nvim
-# ln -sf ~/dotfile/.zprezto ~/
+
 ln -sf ~/dotfile/.zshrc ~/
-ln -sf ~/dotfile/.tmux.conf ~/
 ln -sf ~/dotfile/.zprofile ~/
 ln -sf ~/dotfile/.aliases ~/
 ln -sf ~/dotfile/.gitignore_global ~/
