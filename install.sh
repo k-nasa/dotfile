f0b89ccbf4085aacd47dd68d000cb1c8fe987cd2
k-nasa <htilcs1115@gmail.com>
2020-02-06 18:25:11 +0900

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
 brew install neovim
 brew install rg
+brew install direnv
+brew install nodenv
+brew install pyenv
+brew install rbenv
+brew insatll exa
+brew install fzf
+
+nodenv install 10.18.1
+nodenv global 10.18.1
+
+pyenv install 3.6.0
+pyenv global 3.6.0
+pip install neovim
