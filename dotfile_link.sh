f5636cdb8b0fa234ed7ff57d5c82c3512066badb
k-nasa <htilcs1115@gmail.com>
2020-03-11 15:20:52 +0900

    rename dotfile_link.sh -> scripts/make_links

--- dotfile_link.sh
+++
@@ -1,8 +0,0 @@
-ln -sf ~/dotfile/nvim ~/.config/nvim
-
-ln -sf ~/dotfile/.zshrc ~/
-ln -sf ~/dotfile/.zprofile ~/
-ln -sf ~/dotfile/.aliases ~/
-ln -sf ~/dotfile/.gitignore_global ~/
-ln -sf ~/dotfile/.gitconfig ~/
-ln -sf ~/dotfile/.commit_template ~/
